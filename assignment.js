0c26b96c529bbe0d3fc4e291a9fe3ee73ffe6704
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-07-22 17:44:12 +0600

    prb

--- assignment.js
+++ assignment.js
@@ -25,7 +25,58 @@ var bed= 3;
 var chairN =1*chair;
 var tableN= 3*table;
 var bedN= 5*bed;
-var result = chairN+tableN+bedN;console.log("You'll be needed", result ,"m^3 of wood")
+var result = chairN+tableN+bedN;
+console.log("You'll be needed", result ,"m^3 of wood")
 
 
 
+var numberOfFloor = 21;
+var sum = sum1+sum2+sum3;
+console.log("You'll need",sum,"bricks");
+
+function brickCalculator( numberOfFloor, sum,sum1,sum2,sum3,total)
+{
+    
+    sum = sum1 + sum2 + sum3;
+    var numberOfFloor = 0;
+
+if(numberOfFloor<=10){
+{
+    for( var i=0; i<=0; i++)
+    var total = i*15000;
+    var sum1 = 0;
+    var sum1 = sum1 + total;
+}
+if(numberOfFloor<=20)
+{
+    for( var i=0; i<=0; i++)
+    var total = i*15000;
+    var sum1 = 0;
+    var sum1 = sum1 + total;
+    for( var i=11; i<=20; i++)
+    var total = i*12000;
+    var sum2 = 0;
+    var sum2 = sum2 + total;
+}
+
+if(numberOfFloor>20)
+{
+    for( var i=0; i<=0; i++)
+    var total = i*15000;
+    var sum1 = 0;
+    var sum1 = sum1 + total;
+    for( var i=11; i<=20; i++)
+    var total = i*12000;
+    var sum2 = 0;
+    var sum2 = sum2 + total;
+    for( var i=21; i>20; i++)
+    var total = i*10000;
+    var sum3 = 0;
+    var sum3 = sum3 + total;
+}
+}
+else { 
+    console.log("Please, give input with proper number")
+}
+
+ }
