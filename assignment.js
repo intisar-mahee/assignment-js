49580324d045bca61bb4c8fd1df1e5b060f11e38
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-07-22 16:55:34 +0600

    woodCalculator

--- assignment.js
+++ assignment.js
@@ -1,7 +1,31 @@
-function feetToMile(feet, mile){
-    feet = 1;
-    mile = feet*0.000189394;
-}
+function feetToMile(feet, mile)
+                                {
+                                    feet = 1;
+                                    mile = feet*0.000189394;
+                                }
 var feet = 100000;
 var mile = feet*0.000189394;
 console.log("The amount of mile is", mile); 
+
+
+
+
+
+
+function woodCalculator(chairN, tableN,bedN, chair, table,)
+                                    {
+                                      chairN=1*chair;
+                                      tableN=3*table;
+                                      bedN=5*bed;
+                                      result= chairN+tableN+bedN;      
+                                    }
+var chair =1;
+var table= 2;
+var bed= 3;
+var chairN =1*chair;
+var tableN= 3*table;
+var bedN= 5*bed;
+var result = chairN+tableN+bedN;console.log("You'll be needed", result ,"m^3 of wood")
+
+
+
